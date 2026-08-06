5fd886df0b489b0fd8bdd853c9462c9107607ffc
f7aditya <adiverma8858@gmail.com>
2026-08-06 19:59:55 +0530

    error fixed

--- backend/test-patch.js
+++ backend/test-patch.js
@@ -6,17 +6,16 @@ const request = require('supertest');
 const { generateToken } = require('./src/core/utils/jwt.ts');
 
 async function run() {
-  await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://admin:admin123@cluster0.je5r9uv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
-  
+  await mongoose.connect(process.env.MONGODB_URI;
   // Find a user
   const user = await User.findOne({});
   if (!user) {
     console.log("No user found");
     process.exit(1);
   }
-  
+
   const token = generateToken({ id: user._id });
-  
+
   const res = await request(app)
     .patch('/api/v1/users/me')
     .set('Authorization', `Bearer ${token}`)
@@ -27,10 +26,10 @@ async function run() {
       skills: ['React', 'Javascript'],
       avatarUrl: 'http://localhost:5001/uploads/1268519.jpg'
     });
-    
+
   console.log("STATUS:", res.status);
   console.log("BODY:", res.body);
-  
+
   process.exit(0);
 }
 
